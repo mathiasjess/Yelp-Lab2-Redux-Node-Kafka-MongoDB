5779049e19e0f7b3f0db4fc6745572731ed1f2a1
mathiasjess <jess.bern.mathias@gmail.com>
2021-01-04 12:54:54 -0800

    updated mongodb atlas url

--- Backend/mongoose.js
+++ Backend/mongoose.js
@@ -2,12 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-// mongoose.connect('mongodb+srv://shivani:shivani@cluster0-50hvz.mongodb.net/test?retryWrites=truee', { useNewUrlParser: true, poolSize: 100 }, (err) => {
-//     if (err) throw err;
-//     console.log("mongoose server running");
-// });
-
-mongoose.connect("mongodb+srv://id:password@yelp.pnr8y.mongodb.net/yelp?retryWrites=true&w=majority",
+mongoose.connect("mongodb atlas url",
     { useNewUrlParser: true, poolSize: 100 }, (err) => {
         if (err) console.log(err);
         else console.log("Connected to MongoDB.")
